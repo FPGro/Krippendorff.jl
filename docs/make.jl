6303b702323cd97b3773b71d60d8d35d98ec8bfc
FPGro <38361879+FPGro@users.noreply.github.com>
2021-01-28 22:44:11 +0100

    Fixed deploy repo path (duh!)

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/FPGro/Krippendorff.jl.git",
+    repo = "github.com/FPGro/Krippendorff.git",
     devbranch = "main"
 )
