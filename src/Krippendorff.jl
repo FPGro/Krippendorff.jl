b7a0e79f7cdad172e0c66b3303371ad2bcf12a34
FPGro <38361879+FPGro@users.noreply.github.com>
2021-01-29 00:05:27 +0100

    insert header

--- src/Krippendorff.jl
+++ src/Krippendorff.jl
@@ -1,4 +1,22 @@
-# TODO insert header
+#
+# Developed for:
+#
+#  C O N S T R U C T I V I S T__ __
+#   _____ ____   ____   /  |/  // /
+#  / ___// __ \ / __ \ / /|_/ // /
+# / /__ / /_/ // / / // /  / // /___
+# \___/ \____//_/ /_//_/  /_//_____/
+#  M A C H I N E   L E A R N I N G
+#
+#
+# A Project by
+# Thomas Schmid | UNIVERSITÄT LEIPZIG
+# www.constructivist.ml
+#
+# Code Author: Florian Große
+# Licence: MIT
+#
+
 """
 I should totally document the module
 """
